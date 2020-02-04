ab6967b9c63f2d7ba36382b382c5b04f6b240145
JoaooVictoorr <jvpiccoli@gmail.com>
2020-02-03 21:10:34 -0300

    Adding Chrome Execution

--- src/main/java/TestGoogle.java
+++ src/main/java/TestGoogle.java
@@ -1,6 +1,7 @@
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TestGoogle {
@@ -9,7 +10,9 @@ public class TestGoogle {
 	public void Test() {
 		
 		System.setProperty("webdriver.gecko.driver", "C:/Users/João Victor/Downloads/geckodriver.exe");
-		WebDriver driver = new FirefoxDriver();
+		System.setProperty("webdriver.chrome.driver", "C:/Users/João Victor/Downloads/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
 		
 		driver.get("http://www.google.com.br");
 		Assert.assertEquals("Google", driver.getTitle());
